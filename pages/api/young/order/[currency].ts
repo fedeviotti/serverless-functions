466d33e3380746da11d7623ade4b4304511d4a36
Federico Viotti <fedeviotti@gmail.com>
2023-01-16 22:31:59 +0100

    feat: implement recurring api

--- pages/api/young/order/[currency].ts
+++ pages/api/young/order/[currency].ts
@@ -1,6 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getHmacFromObject } from "utils/getHmacFromObject";
+import { OrderResponse } from "./types";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(400).send("Method not allowed");
+  }
+
+  if(req.headers.authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
+    return res.status(401).send("Unauthorized");
+  }
+
   const { currency, volume } = req.query;
-  res.status(200).json({ currency, volume });
+
+  const body = {
+    timestamp: Math.floor(Date.now() / 1000),
+    recvWindow: 100000,
+    trade: currency,
+    market: "EUR",
+    side: "BUY",
+    type: "MARKET",
+    volume
+  };
+  try {
+    const response = await fetch("https://api.youngplatform.com/api/v3/placeOrder", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        apiKey: process.env.YOUNG_API_PUBLIC_KEY || "",
+        hmac: getHmacFromObject(body),
+      },
+    });
+    const result = await response.json() as OrderResponse;
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(500).json({ error: `An error occurred: ${error.message}` })
+  }
 }
+
---
+++ pages/api/young/order/types.ts
@@ -0,0 +1,30 @@
+type Trade = {
+  id: number,
+  orderID: number,
+  baseCurrency: string,
+  quoteCurrency: string,
+  side: string,
+  taker: boolean,
+  volume: number,
+  rate: number,
+  executionDate: string,
+}
+
+export type OrderResponse = {
+  orderID: number,
+  cid: number,
+  baseCurrency: string,
+  quoteCurrency: string,
+  type: string,
+  side: string,
+  volume: number,
+  rate: number,
+  amount: number,
+  brokerage: number,
+  pendingVolume: number,
+  orderStatus: boolean,
+  orderPlacementDate: string,
+  orderConfirmDate: string,
+  trades: Trade[],
+  fee: string,
+}
