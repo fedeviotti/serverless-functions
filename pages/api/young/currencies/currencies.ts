2498ccbb3bc1a59c0a99e83bcede8f5b443ec1c1
Federico Viotti <fedeviotti@gmail.com>
2023-01-19 23:24:56 +0100

    chore(api): add mail receiver

--- pages/api/young/currencies/currencies.ts
+++ pages/api/young/currencies/currencies.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
+    to: "fedeviotti@gmail.com, koine.eirene@gmail.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
