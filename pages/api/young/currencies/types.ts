2cbdee3c297dac77378064c8e3637c12859873ba
Federico Viotti <fedeviotti@gmail.com>
2023-01-20 22:01:34 +0100

    chore: Currency type for currencies.ts

--- pages/api/young/currencies/types.ts
+++ pages/api/young/currencies/types.ts
@@ -1,12 +1,5 @@
 export type Currency = {
   name: string;
   symbol: string,
-  activeBuy: boolean,
-  activeSell: boolean,
-  activeDeposit: boolean,
-  activeWithdraw: boolean,
-  decimalPrecision: number,
-  withdrawFee: number,
-  withdrawFeePercentage: number,
-  withdrawFixedFee: boolean
+  price: number,
 }
